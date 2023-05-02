dcabeba59a690587837e23969b10cb05dfbdf3fe
JungWKim <lionmini@naver.com>
2023-05-02 14:58:32 +0900

    .

--- setup_kubeflow.sh
+++ setup_kubeflow.sh
@@ -3,11 +3,11 @@
 MASTER_IP=
 
 # download kubeflow manifest repository
+cd ~
 git clone https://github.com/kubeflow/manifests.git -b v1.7-branch
-mv manifests ${ADMIN_HOME}/
 
 # enable kubeflow to be accessed through https (1)
-cat << EOF >> ${ADMIN_HOME}/manifests/common/istio-1-16/kubeflow-istio-resources/base/kf-istio-resources.yaml
+cat << EOF >> ${HOME}/manifests/common/istio-1-16/kubeflow-istio-resources/base/kf-istio-resources.yaml
     tls:
       httpsRedirect: true
   - hosts:
@@ -23,7 +23,7 @@ cat << EOF >> ${ADMIN_HOME}/manifests/common/istio-1-16/kubeflow-istio-resources
 EOF
 
 # enable kubeflow to be accessed through https (2)
-sed -i "s/true/false/g" ${ADMIN_HOME}/manifests/apps/jupyter/jupyter-web-app/upstream/base/deployment.yaml
+sed -i "s/true/false/g" ${HOME}/manifests/apps/jupyter/jupyter-web-app/upstream/base/deployment.yaml
 
 # download kustomize 3.2.0 which is stable with kubeflow 1.5.0 then copy it into /bin/bash
 wget https://github.com/kubernetes-sigs/kustomize/releases/download/kustomize%2Fv5.0.0/kustomize_v5.0.0_linux_amd64.tar.gz
